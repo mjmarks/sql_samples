027d007b43e66c69d01c220407e33fd4e6c5fbdc
Michael Marks <michael.j.marks@outlook.com>
2017-08-22 23:11:20 -0700

    Update retrieve_producer_data.sql

--- retrieve_producer_data.sql
+++ retrieve_producer_data.sql
@@ -32,7 +32,7 @@ JOIN db1.dbo.table2 b ON a.COCE_ID = b.COCE_ID
 JOIN db1.dbo.table3 c ON a.BLEI_CK = c.BLEI_CK
 WHERE a.BLBL_DUE_DT BETWEEN @BeginDate AND @EndDate AND
       b.COCE_TERM_DT >= @BeginDate AND
-	    c.BEIN_TERM_DT >= @BeginDate
+      c.BEIN_TERM_DT >= @BeginDate
 --=========================================================
 --Step 3: Establish a list of Groups, as well as their respective State and
         --Group Size values.
@@ -45,12 +45,12 @@ INTO #temptable2
 FROM db2.dbo.table4 d, db2.dbo.table4 e
 WHERE d.GRGR_CK = e.GRGR_CK AND
       d.GRGR_ID = e.GRGR_ID AND
-	    d.ELE_TERM_DT >= @BeginDate AND
-	    e.ELE_TERM_DT >= @BeginDate AND
-	    d.ELEMENT = <VALUE> AND
-	    e.ELEMENT = <VALUE> AND
-	    d.ELE_VALUE = @State AND
-	    e.ELE_VALUE = @GroupSize
+      d.ELE_TERM_DT >= @BeginDate AND
+      e.ELE_TERM_DT >= @BeginDate AND
+      d.ELEMENT = <VALUE> AND
+      e.ELEMENT = <VALUE> AND
+      d.ELE_VALUE = @State AND
+      e.ELE_VALUE = @GroupSize
 --=========================================================
 --Step 4: Establish a list of Groups, as well as their respective Class Plan
         --identifiers, Product Categories, and Funding Types.
@@ -63,7 +63,7 @@ INTO #temptable3
 FROM db2.dbo.table5 f
 WHERE f.ELEMENT = <VALUE> AND
       f.ELE_TERM_DT >= @BeginDate AND
-	    f.ELE_VALUE = @FundTyp
+      f.ELE_VALUE = @FundTyp
 --=========================================================
 --Step 5: Establish a more complete composition of Producer Billing and
         --Commissions data.
@@ -73,17 +73,17 @@ SELECT g.COCE_ID,
        g.CSPI_ID,
        g.PDPD_ID,
        (CASE WHEN h.PLDS_DESC LIKE <VALUE> THEN NULL
-		         WHEN h.PLDS_DESC LIKE <VALUE> THEN NULL
-	           WHEN h.PLDS_DESC LIKE <VALUE> THEN NULL
+	     WHEN h.PLDS_DESC LIKE <VALUE> THEN NULL
+	     WHEN h.PLDS_DESC LIKE <VALUE> THEN NULL
              ELSE h.PLDS_DESC
-	      END) AS PLDS_DESC,
-	     j.CSPD_CAT,
+	END) AS PLDS_DESC,
+       j.CSPD_CAT,
        g.GRGR_CK,
        g.GRGR_ID,
        g.COEC_SOURCE_AMT,
        g.COEC_COMM_AMT_PAID,
        i.STATE,
-	     i.GRP_SIZ,
+       i.GRP_SIZ,
        j.ELE_VALUE
 INTO #temptable4
 FROM #temptable1 g
@@ -102,7 +102,7 @@ SELECT k.STATE,
        k.COCE_NAME,
        k.CSPD_CAT,
        SUM(k.COEC_SOURCE_AMT) AS PREM_AMT,
-	     SUM(k.COEC_COMM_AMT_PAID) AS COMM_AMT,
+       SUM(k.COEC_COMM_AMT_PAID) AS COMM_AMT,
        subquery2.PLCY_CNT,
        subquery5.MEM_CNT
 FROM #temptable4 k
@@ -111,18 +111,18 @@ JOIN (SELECT STATE,
              COCE_ID,
              CSPD_CAT,
              SUM(PLCY_CNT) AS PLCY_CNT
-	    FROM (SELECT DISTINCT STATE,
-                            GRP_SIZ,
-                            COCE_ID,
-                            CSPI_ID,
-                            CSPD_CAT,
-                            COUNT(DISTINCT PLDS_DESC) AS PLCY_CNT
-            FROM #temptable4
-            GROUP BY STATE, GRP_SIZ, COCE_ID, CSPI_ID, CSPD_CAT) AS subquery1
-	    GROUP BY STATE, GRP_SIZ, COCE_ID, CSPD_CAT) AS subquery2 ON k.STATE = subquery2.STATE AND
-	                                                                k.GRP_SIZ = subquery2.GRP_SIZ AND
-	                                                                k.COCE_ID = subquery2.COCE_ID AND
-														                                      k.CSPD_CAT = subquery2.CSPD_CAT
+       FROM (SELECT DISTINCT STATE,
+                             GRP_SIZ,
+                             COCE_ID,
+                             CSPI_ID,
+                             CSPD_CAT,
+                             COUNT(DISTINCT PLDS_DESC) AS PLCY_CNT
+             FROM #temptable4
+             GROUP BY STATE, GRP_SIZ, COCE_ID, CSPI_ID, CSPD_CAT) AS subquery1
+	GROUP BY STATE, GRP_SIZ, COCE_ID, CSPD_CAT) AS subquery2 ON k.STATE = subquery2.STATE AND
+	                                                            k.GRP_SIZ = subquery2.GRP_SIZ AND
+	                                                            k.COCE_ID = subquery2.COCE_ID AND
+							            k.CSPD_CAT = subquery2.CSPD_CAT
 JOIN (SELECT subquery3.STATE,
              subquery3.GRP_SIZ,
              subquery3.COCE_ID,
@@ -143,15 +143,15 @@ JOIN (SELECT subquery3.STATE,
                                  MEME_CK
                  FROM db1.dbo.table7
                  WHERE MEPE_TERM_DT >= @BeginDate AND
-	                     MEPE_ELIG_IND = <VALUE> AND
-		                   CSPD_CAT IN (<VALUES>)) AS subquery4 ON subquery3.GRGR_CK = subquery4.GRGR_CK AND
-	                                                             subquery3.CSPI_ID = subquery4.CSPI_ID AND
-										                                           subquery3.PDPD_ID = subquery4.PDPD_ID AND
-														                                   subquery3.CSPD_CAT = subquery4.CSPD_CAT
+	               MEPE_ELIG_IND = <VALUE> AND
+		       CSPD_CAT IN (<VALUES>)) AS subquery4 ON subquery3.GRGR_CK = subquery4.GRGR_CK AND
+	                                                       subquery3.CSPI_ID = subquery4.CSPI_ID AND
+					                       subquery3.PDPD_ID = subquery4.PDPD_ID AND
+							       subquery3.CSPD_CAT = subquery4.CSPD_CAT
       GROUP BY subquery3.STATE, subquery3.GRP_SIZ, subquery3.COCE_ID, subquery3.CSPD_CAT) AS subquery5 ON k.STATE = subquery5.STATE AND
-	                                                                                                        k.GRP_SIZ = subquery5.GRP_SIZ AND
-	                                                                                                        k.COCE_ID = subquery5.COCE_ID AND
-										                                                                                      k.CSPD_CAT = subquery5.CSPD_CAT
+	                                                                                                  k.GRP_SIZ = subquery5.GRP_SIZ AND
+	                                                                                                  k.COCE_ID = subquery5.COCE_ID AND
+										                          k.CSPD_CAT = subquery5.CSPD_CAT
 WHERE CSPD_CAT = @ProdCat
 GROUP BY k.STATE, k.GRP_SIZ, k.ELE_VALUE, k.COCE_ID, k.COCE_NAME, k.CSPD_CAT, subquery2.PLCY_CNT, subquery5.MEM_CNT
 ORDER BY k.COCE_NAME
